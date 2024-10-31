b22bafc34c41443e2b2bb70dfe88eb2cc0e90b22
jainam12121 <148000472+jainam12121@users.noreply.github.com>
2024-10-31 22:26:42 +0530

    processor.py

--- processor.py
+++ processor.py
@@ -4,7 +4,7 @@ import yaml
 import logging
 from ttstokenizer import TTSTokenizer
 
-Set up logging
+#Set up logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
