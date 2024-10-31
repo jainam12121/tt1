71fd372c5ffc57f3bc998bfed6bc8757cfc8e48e
jainam12121 <148000472+jainam12121@users.noreply.github.com>
2024-10-31 14:22:27 +0530

    processor.py

--- processor.py
+++ processor.py
@@ -47,19 +47,14 @@ tokenizer = TTSTokenizer(config["token"]["list"])
 
 def pre_process(text):
     """Tokenizes input text."""
-    print("In Custom pre_process method")
-    print("Input text:", text)
     tokenized_input = tokenizer(text)
-    print("Tokenized input:", tokenized_input)
     return tokenized_input
 
 def post_process(output):
     """Processes model output and saves it as a .wav file."""
-    print("In Custom post_process method")
     audio_data = output[0]
     output_file = "out.wav"
     sf.write(output_file, audio_data, 22050)
-    print("Audio written to:", output_file)
     return output_file
 
 
